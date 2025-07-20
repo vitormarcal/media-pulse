3ab93f1c2e21c05994ad068bda3f93b21cdaffa3
Vítor Marçal <codehero@marcal.dev>
2025-07-20 14:31:29 -0300

    fix(plex) Fix plex webhook

--- server/src/main/kotlin/dev/marcal/mediapulse/server/controller/WebhookController.kt
+++ server/src/main/kotlin/dev/marcal/mediapulse/server/controller/WebhookController.kt
@@ -3,8 +3,8 @@ package dev.marcal.mediapulse.server.controller
 import dev.marcal.mediapulse.server.controller.dto.WebhookDTO
 import dev.marcal.mediapulse.server.service.WebhookEventService
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
@@ -19,9 +19,9 @@ class WebhookController(
         private val logger = LoggerFactory.getLogger(this::class.java)
     }
 
-    @PostMapping("/plex")
+    @PostMapping("/plex", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun webhook(
-        @RequestBody payload: String,
+        @RequestPart("payload") payload: String,
         @RequestPart("thumb", required = false) thumb: MultipartFile?,
     ) {
         logger.info("Webhook received for provider: plex. Thumb present: ${thumb != null}. Payload: $payload")
--- server/src/test/kotlin/dev/marcal/mediapulse/server/controller/WebhookControllerIT.kt
+++ server/src/test/kotlin/dev/marcal/mediapulse/server/controller/WebhookControllerIT.kt
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.mock.web.MockMultipartFile
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 class WebhookControllerIT : MediapulseServerApplicationTests() {
@@ -20,14 +21,70 @@ class WebhookControllerIT : MediapulseServerApplicationTests() {
     @Nested
     inner class WebhookControllerTests {
         @Test
-        fun `should return OK when webhook is received`() {
-            val payload = """{"key":"value"}"""
+        fun `should return OK when webhook multipart is received`() {
+            val payloadJson =
+                """
+                {
+                  "event": "media.play",
+                  "user": true,
+                  "owner": true,
+                  "Account": {
+                    "id": 1,
+                    "thumb": "https://plex.tv/users/1022b120ffbaa/avatar?c=1465525047",
+                    "title": "elan"
+                  },
+                  "Server": {
+                    "title": "Office",
+                    "uuid": "54664a3d8acc39983675640ec9ce00b70af9cc36"
+                  },
+                  "Player": {
+                    "local": true,
+                    "publicAddress": "200.200.200.200",
+                    "title": "Plex Web (Safari)",
+                    "uuid": "r6yfkdnfggbh2bdnvkffwbms"
+                  },
+                  "Metadata": {
+                    "librarySectionType": "artist",
+                    "ratingKey": "1936545",
+                    "key": "/library/metadata/1936545",
+                    "parentRatingKey": "1936544",
+                    "grandparentRatingKey": "1936543",
+                    "guid": "com.plexapp.agents.plexmusic://gracenote/track/7572499-91016293BE6BF7F1AB2F848F736E74E5/7572500-3CBAE310D4F3E66C285E104A1458B272?lang=en",
+                    "librarySectionID": 1224,
+                    "type": "track",
+                    "title": "Love The One You're With",
+                    "grandparentKey": "/library/metadata/1936543",
+                    "parentKey": "/library/metadata/1936544",
+                    "grandparentTitle": "Stephen Stills",
+                    "parentTitle": "Stephen Stills",
+                    "summary": "",
+                    "index": 1,
+                    "parentIndex": 1,
+                    "ratingCount": 6794,
+                    "thumb": "/library/metadata/1936544/thumb/1432897518",
+                    "art": "/library/metadata/1936543/art/1485951497",
+                    "parentThumb": "/library/metadata/1936544/thumb/1432897518",
+                    "grandparentThumb": "/library/metadata/1936543/thumb/1485951497",
+                    "grandparentArt": "/library/metadata/1936543/art/1485951497",
+                    "addedAt": 1000396126,
+                    "updatedAt": 1432897518
+                  }
+                }
+                """.trimIndent()
+
+            val payloadPart =
+                MockMultipartFile(
+                    "payload",
+                    "payload",
+                    MediaType.APPLICATION_JSON_VALUE,
+                    payloadJson.toByteArray(),
+                )
 
             mockMvc
                 .perform(
-                    post("/webhook/plex")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(payload),
+                    multipart("/webhook/plex")
+                        .file(payloadPart)
+                        .contentType(MediaType.MULTIPART_FORM_DATA),
                 ).andExpect(status().isOk)
 
             val list = webhookEventRepository.findAll()
