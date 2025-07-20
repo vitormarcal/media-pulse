df8a3ce6ba3670c2554e5166580bb25f8c264c16
Vítor Marçal <codehero@marcal.dev>
2025-07-20 14:50:24 -0300

    fix(plex) Fix migartion

--- server/src/main/resources/db/migration/V1__create_table_webhook_events.sql
+++ server/src/main/resources/db/migration/V1__create_table_webhook_events.sql
@@ -1,6 +1,6 @@
 CREATE TABLE webhook_events
 (
-    id              SERIAL PRIMARY KEY,
+    id              BIGSERIAL PRIMARY KEY,
     provider        VARCHAR(100) NOT NULL,
     event_timestamp TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
     payload         text        NOT NULL,
