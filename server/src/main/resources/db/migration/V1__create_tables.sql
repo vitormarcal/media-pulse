32bc6487dd79f1343ca741d9ef1c5ae2617585d8
Vítor Marçal <codehero@marcal.dev>
2025-07-26 22:44:32 -0300

    fix(migration) Fix typo in migration

--- server/src/main/resources/db/migration/V1__create_tables.sql
+++ server/src/main/resources/db/migration/V1__create_tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE event_sources
     id            BIGSERIAL PRIMARY KEY,
     provider      VARCHAR(100)             NOT NULL,
     created_at    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    update_at     TIMESTAMP WITH TIME ZONE,
+    updated_at     TIMESTAMP WITH TIME ZONE,
     payload       TEXT                     NOT NULL,
     fingerprint   VARCHAR(255) UNIQUE      NOT NULL,
     status        VARCHAR(20)              NOT NULL,
