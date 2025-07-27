19088949491d17de96d0e037effa0510b42bb0a5
Vítor Marçal <codehero@marcal.dev>
2025-07-26 23:05:08 -0300

    fix(migration) Fix lastViewedAt is optional

--- server/src/main/kotlin/dev/marcal/mediapulse/server/controller/dto/WebhookDTO.kt
+++
@@ -1,9 +0,0 @@
-package dev.marcal.mediapulse.server.controller.dto
-
-import org.springframework.web.multipart.MultipartFile
-
-data class WebhookDTO(
-    val provider: String,
-    val payload: String,
-    val file: MultipartFile? = null,
-)
