2ecf52e039eb50ef53311c8f459784ee7f393316
Vítor Marçal <codehero@marcal.dev>
2025-07-26 22:55:24 -0300

    fix(migration) Fix wrong jpa mapping

--- server/src/main/kotlin/dev/marcal/mediapulse/server/model/music/CanonicalTrack.kt
+++ server/src/main/kotlin/dev/marcal/mediapulse/server/model/music/CanonicalTrack.kt
@@ -3,17 +3,17 @@ package dev.marcal.mediapulse.server.model.music
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import jakarta.persistence.UniqueConstraint
 
 @Entity
 @Table(
     name = "canonical_tracks",
-    uniqueConstraints = [UniqueConstraint(columnNames = ["canonical_id", "canonical_type"])],
 )
 data class CanonicalTrack(
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
     @Column(name = "canonical_id", nullable = false)
     val canonicalId: String,
